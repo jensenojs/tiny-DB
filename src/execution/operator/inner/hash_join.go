2d5688c4c5fb909c82951a98330c0595f02577f8
Jensen <jensenojs@qq.com>
2022-08-25 14:22:59 +0800

    feat : Materialize for hash join
     * Execute for hash join still working on...

--- src/execution/operator/inner/hash_join.go
+++ src/execution/operator/inner/hash_join.go
@@ -7,13 +7,18 @@ import (
 	"tiny-db/src/storage"
 )
 
+type mapValue struct {
+	ithChunk   int // mark which chunk store the value
+	idxInChunk int // mark the idx in this chunk
+}
+
 type innerHashState struct {
-	l sync.Mutex  // this mutex is used in join state in parallel build the hash table.
-	b *storage.DataChunk // build table
+	l  sync.Mutex           // this mutex is used in join state in parallel build the hash table.
+	bs []*storage.DataChunk // As we can't guarantee that the data that the build table can match is in one chunk, so we need store it in slice.
 
 	// TODO(Jensen) : Need a better way to implement hash map, to support different types of Value and multiple keys
 	// For now we have to use value.ToString() as keys, and the value of map is used to record the idxs of key appear
-	m map[string][]int
+	m map[string]*mapValue
 }
 
 type InnerHash struct {
@@ -21,6 +26,13 @@ type InnerHash struct {
 
 	lchild executor.Operator
 	rchild executor.Operator
+}
+
+func newMapValue(ith, idx int) *mapValue {
+	return &mapValue{
+		ithChunk:   ith,
+		idxInChunk: idx,
+	}
 }
 
 func NewInnerJoin(mChunk []*storage.DataChunk, lchild, rchild executor.Operator) *InnerHash {
@@ -34,7 +46,8 @@ func NewInnerJoin(mChunk []*storage.DataChunk, lchild, rchild executor.Operator)
 func (i *InnerHash) InitLocalStateForMaterialize() (any, error) {
 	s := &innerHashState{
 		// TODO(Jensen): Hard code for now, Need Customized hash map
-		m: make(map[string][]int),
+		m:  make(map[string]*mapValue),
+		bs: make([]*storage.DataChunk, 0),
 	}
 	return s, nil
 }
@@ -42,12 +55,16 @@ func (i *InnerHash) InitLocalStateForMaterialize() (any, error) {
 // the Build state of inner hash join.
 func (i *InnerHash) Materialize(build *storage.DataChunk, state any) error {
 	ops := (state).(innerHashState)
-	ops.b = build
 	hashMap := ops.m
+	buildChunks := ops.bs
+	ithChunk := len(buildChunks)
+	buildChunks = append(buildChunks, build)
+
 	for i := 0; i < build.Count(); i++ {
+		// TODO(Jensen): 1. Equal Conditional Expression to build the table. 2. select which col to build
 		key := build.Cols[0].GetValue(i).ToString()
-		// TODO(Jensen): Need Equal Conditional Expression to build the table.
-		hashMap[key] = append(hashMap[key], i)
+		value := newMapValue(ithChunk, i)
+		hashMap[key] = value
 	}
 
 	return nil
@@ -56,23 +73,52 @@ func (i *InnerHash) Materialize(build *storage.DataChunk, state any) error {
 // the Probe state of inner hash join.
 func (i *InnerHash) Execute(probe, result *storage.DataChunk, state any) error {
 	ops := (state).(innerHashState)
+	// buildChunks := ops.bs
 	hashMap := ops.m
-	sels := vector.NewSelVector()
+	probeSels := vector.NewSelVector()
+	buildSels := make([]*mapValue, 0)
 
+	// probe
 	resultCount := 0
 	for i := 0; i < probe.Count(); i++ {
 		// TODO(Jensen) : Need expression calculation to decide whether this key in hashMap
 		// also need to decide which column
 		// chosen to be key, now just simply hard code and use first column as key.
 		key := probe.Cols[0].GetValue(i).ToString()
-		if idxs, ok := hashMap[key]; ok {
-			sels.SetIndex(resultCount, idxs[0])
+		if mv, ok := hashMap[key]; ok {
+			probeSels.SetIndex(resultCount, i)
+			buildSels = append(buildSels, mv)
 			resultCount++
 		}
 	}
 
-	// Need to concate two datachunk but not SLice from input.
-	result.Slice(probe, sels, resultCount)
+	// Hard code : Currently join all column from probe and build table.
+	// probe table
+	for i := 0; i < probe.ColumnCount(); i++ {
+		pcol := probe.Cols[i]
+		pcol.Slice(probeSels, resultCount)
+		result.Cols[i] = pcol
+	}
+
+	// build table
+	// bias := probe.ColumnCount()
+	// for _, mv := range buildSels {
+	// 	ith, idx := mv.ithChunk, mv.idxInChunk
+	// 	for i := 0; i < buildChunks[0].ColumnCount(); i++ {
+	// 		bcol := result.Cols[i+bias]
+	// 		bv := buildChunks[ith].Cols[i].GetValue(idx)
+	// 		ptype := bcol.GetPhyType()
+	// 		if ptype == types.INT32 {
+	// 			rawVec := vector.GetColumn[int32](bcol)
+	// 			rawVec[idx] = bv
+	// 		} else {
+	// 			return errors.New(i.Op_type.String() + ": Unspport phyType")
+	// 		}
+
+	// 	}
+	// }
+
+	result.SetCount(resultCount)
 
 	return nil
 }
